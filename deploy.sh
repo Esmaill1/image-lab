6a1efe636683a92488037dfe950a081ebdbc8009
Mohamed Esmail <moh12345678912mmo@gmail.com>
2025-12-17 16:46:47 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,24 +3,19 @@ set -e
 
 docker build -t image-lab:latest .
 
-# 3. Stop and remove the existing container (if it exists)
-
 echo "Removing old container..."
 docker stop image-lab-container || true
 docker rm image-lab-container || true
 
-# 4. Run the new container
-
 echo "Starting new container..."
 docker run -d \
   --name image-lab-container \
-      -e NEW_RELIC_LICENSE_KEY=$NEW_RELIC_LICENSE_KEY \
-      -e NEW_RELIC_APP_NAME=$NEW_RELIC_APP_NAME \
-      -p 5000:5000 \
   --restart unless-stopped \
+  -e NEW_RELIC_LICENSE_KEY="$NEW_RELIC_LICENSE_KEY" \
+  -e NEW_RELIC_APP_NAME="$NEW_RELIC_APP_NAME" \
+  -e NEW_RELIC_DISTRIBUTED_TRACING_ENABLED=true \
   -p 5000:5000 \
   image-lab:latest
 
 echo "Deployment successful!"
-
 docker image prune -f
